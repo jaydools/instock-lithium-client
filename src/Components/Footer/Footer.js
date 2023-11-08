ae68c6dd0ebd53743fca57810e0f46c3e20f9683
marcellui <marcellui123@gmail.com>
2023-11-08 14:11:27 -0500

    finish footer and css

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.scss";
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer>
+            <p className="footer">© InStock Inc. All Rights Reserved.</p>
+        </footer>
+    );
 }
 
 export default Footer;
