cbf04f2d8d1862ca4c2da1f5dd09ae682b71c701
Alex Howley <howley.alex@gmail.com>
2023-11-08 15:05:17 -0600

    add grid component html WIP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Header from "./Components/Header/Header";
+// import Footer from "./Components/Footer/Footer";
+// import Warehouses from "./Pages/Warehouses/Warehouses";
+// import Inventory from "./Pages/InventoryPage/InventoryPage";
+
+// function App() {
+//     return (
+//         <BrowserRouter>
+//             <Header />
+//             <Routes>
+//                 <Route path="/" element={<Warehouses />} />
+//                 <Route path="/warehouses" element={<Warehouses />} />
+//                 <Route path="/warehouses/:id" element={<Warehouses />} />
+//                 <Route path="/inventory" element={<Inventory />} />
+//                 <Route path="/inventory/:id" element={<Inventory />} />
+//             </Routes>
+//             <Footer />
+//         </BrowserRouter>
+//     );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Warehouses from "./Pages/Warehouses/Warehouses";
 import Inventory from "./Pages/InventoryPage/InventoryPage";
+import Grid from "./Components/Grid/Grid";
 
 function App() {
     return (
@@ -16,6 +41,17 @@ function App() {
                 <Route path="/inventory/:id" element={<Inventory />} />
             </Routes>
             <Footer />
+
+            <Grid
+                fieldNames={["field1", "field2", "field3"]}
+                records={[
+                    { id: "test", field1: "value1", field2: "value2", field3: "value3" },
+                    { id: "test", field1: "value1", field2: "value2", field3: "value3" },
+                    { id: "test", field1: "value1", field2: "value2", field3: "value3" },
+                    { id: "test", field1: "value1", field2: "value2", field3: "value3" },
+                    { id: "test", field1: "value1", field2: "value2", field3: "value3" },
+                ]}
+            />
         </BrowserRouter>
     );
 }
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,8 +1,72 @@
 import React from "react";
 import "./Grid.scss";
+import { Link } from "react-router-dom";
+import editIcon from "../../Assets/Images/edit-24px.svg";
+import deleteIcon from "../../Assets/Images/delete_outline-24px.svg";
 
-function Grid() {
-    return <div>Grid</div>;
+function Grid({ fieldNames, records, linkToDetailsPage, onEdit, onDelete }) {
+    return (
+        <section className="grid">
+            <ul className="grid__headers">
+                {fieldNames.map((field, i) => (
+                    <li key={i} className="grid__header">
+                        {field}
+                    </li>
+                ))}
+            </ul>
+            <ul className="grid__rows">
+                {records.map(record => (
+                    <li key={record.id} className="grid__row">
+                        <dl className="grid__fields">
+                            {fieldNames.map((field, i) => (
+                                <div className="grid__field">
+                                    <dt className="grid__key">{field}</dt>
+                                    {i === 0 ? (
+                                        <dd className="grid__value">
+                                            <Link
+                                                className="grid__details-link"
+                                                to={linkToDetailsPage}
+                                            >
+                                                {record[field]}
+                                            </Link>
+                                        </dd>
+                                    ) : (
+                                        <dd className="grid__value">{record[field]}</dd>
+                                    )}
+                                </div>
+                            ))}
+                        </dl>
+                        <div className="grid__buttons">
+                            <button
+                                type="button"
+                                name="delete"
+                                className="grid__button"
+                                onClick={onEdit}
+                            >
+                                <img
+                                    src={editIcon}
+                                    alt="Icon of a trash can, indicating the delete button."
+                                    className="grid__button-image"
+                                />
+                            </button>
+                            <button
+                                type="button"
+                                name="edit"
+                                className="grid__button"
+                                onClick={onDelete}
+                            >
+                                <img
+                                    src={deleteIcon}
+                                    alt="Icon of a pencil, indicating the edit button."
+                                    className="grid__button-image"
+                                />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 }
 
 export default Grid;
