de4950beb71c7f688f4bfd2f92da3159455e777d
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-09 15:57:31 -0500

    props passed

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -4,7 +4,10 @@ import back from "../../Assets/Images/arrow_back-24px.svg";
 import { ReactComponent as Edit } from "../../Assets/Images/edit-24px.svg";
 import { Link } from "react-router-dom";
 
-function Title() {
+function Title(props) {
+    const { title } = props;
+    const { edit } = props;
+
     return (
         <div>
             <div className="title">
@@ -14,13 +17,13 @@ function Title() {
                             <img src={back} alt="back button" />
                         </button>
                     </Link>
-                    <h1 className="title__header">Title</h1>
+                    <h1 className="title__header">{title}</h1>
                 </div>
                 <div>
                     <Link to="/inventory-item-edit">
                         <button className="title__right">
                             <Edit className="title__edit" />
-                            <p className="title__right--hidden">Edit</p>
+                            <p className="title__right--hidden">{edit}</p>
                         </button>
                     </Link>
                 </div>
