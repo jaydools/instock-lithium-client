d9d9a128b0bbd2915fb63180703df5ac6859056b
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-09 10:39:51 -0500

    title component finished
    underline for link should be gone with global styling

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Title.scss";
+import back from "../../Assets/Images/arrow_back-24px.svg";
+import { ReactComponent as Edit } from "../../Assets/Images/edit-24px.svg";
+import { Link } from "react-router-dom";
 
 function Title() {
-    return <div>Title</div>;
+    return (
+        <div>
+            <div className="title">
+                <div className="title__left">
+                    <Link to="/inventory-list" className="title__link">
+                        <button className="title__back">
+                            <img src={back} alt="back button" />
+                        </button>
+                    </Link>
+                    <h1>Title</h1>
+                </div>
+                <div>
+                    <Link to="/inventory-item-edit">
+                        <button className="title__right">
+                            <Edit className="title__edit" />
+                            <p className="title__right--hidden">Edit</p>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Title;
--- src/Pages/Warehouses/Warehouses.js
+++ src/Pages/Warehouses/Warehouses.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Warehouses.scss";
 import InventoryItemDetails from "../../Components/InventoryItemDetails/InventoryItemDetails";
+import Title from "../../Components/Title/Title";
 
 function Warehouses() {
     return (
         <body>
             {/* INSERT YOUR COMPONENT HERE TO PREVIEW */}
+            <Title />
             <InventoryItemDetails />
         </body>
     );
