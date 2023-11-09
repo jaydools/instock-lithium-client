4bc9a6b5932ee5a19d334c45dcee663b4bc65e88
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-09 13:09:40 -0500

    styling and linking done
    need to make dynamic

--- src/Components/InventoryItemDelete/InventoryItemDelete.js
+++ src/Components/InventoryItemDelete/InventoryItemDelete.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./InventoryItemDelete.scss";
 import close from "../../Assets/Images/close-24px.svg";
+import { Link } from "react-router-dom";
 
 function InventoryItemDelete() {
     return (
         <div className="delete-container">
             <div className="close">
-                <img className="close__img" src={close} />
+                <Link to="/inventory-list" className="close__link">
+                    <button className="close__button">
+                        <img className="close__img" src={close} />
+                    </button>
+                </Link>
             </div>
 
             <div className="main">
@@ -18,8 +23,13 @@ function InventoryItemDelete() {
                     </p>
                 </div>
                 <div className="delete-button">
-                    <button className="delete-button__cancel">Cancel</button>
-                    <button className="delete-button__delete">Delete</button>
+                    <Link to="/inventory-list" className="delete-button__link">
+                        <button className="delete-button__cancel">Cancel</button>
+                    </Link>
+                    <Link to="/inventory-list" className="delete-button__link">
+                        {/*  SEND DELETE REQUEST */}
+                        <button className="delete-button__delete">Delete</button>
+                    </Link>
                 </div>
             </div>
         </div>
