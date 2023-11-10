fce5306ec8397c478aff86407e10fe3d4046618f
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-09 16:37:55 -0800

    adding state to the inventory list for each warehouse

--- src/Components/InventoryList/InventoryList.js
+++ src/Components/InventoryList/InventoryList.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-function InventoryList() {
-    return <div>InventoryList</div>;
-}
+function InventoryList({ id }) {
+    const [warehouseIdInventory, setWarehouseIdInventory] = useState([]);
 
+    useEffect(() => {
+        const fetchWarehouseIdInventories = async id => {
+            try {
+                const response = await axios.get(
+                    `${process.env.REACT_APP_BACKEND_URL}/api/warehouses/${id}/inventories`,
+                );
+                console.log(response);
+                setWarehouseIdInventory(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchWarehouseIdInventories(id);
+    }, [id]);
+    return (
+        <>
+            {warehouseIdInventory ? (
+                <>
+                    <div>title</div>
+                    <h3>{warehouseIdInventory.item_name}</h3>
+                </>
+            ) : (
+                <p>"loading inventory..."</p>
+            )}
+        </>
+    );
+}
 export default InventoryList;
