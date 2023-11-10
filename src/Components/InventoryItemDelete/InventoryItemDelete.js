caf7558028a3ea88ff9dc3e3a700cf021ca3ca8b
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-10 16:11:48 -0500

    finished ticket
    waiting for feature 20 to be finished to make sure its intergrated properly

--- src/Components/InventoryItemDelete/InventoryItemDelete.js
+++ src/Components/InventoryItemDelete/InventoryItemDelete.js
@@ -1,34 +1,48 @@
 import React from "react";
 import "./InventoryItemDelete.scss";
 import close from "../../Assets/Images/close-24px.svg";
-import { Link } from "react-router-dom";
+import axios from "axios";
+const baseURL = "http://localhost:8080/api/";
 
-function InventoryItemDelete(/* maybe pass prop here "onClose" */) {
+function InventoryItemDelete({ inventoryData, onClose }) {
+    const handleDelete = async () => {
+        try {
+            let res = await axios.delete(`${baseURL}inventories/${inventoryData.id}`);
+        } catch (err) {
+            console.error("unknown error... ready to explode", err);
+        }
+
+        onClose();
+    };
+
     return (
         <div className="delete-container">
             <div className="close">
-                <Link to="/inventory-list" className="close__link">
-                    <button className="close__button">
+                <div to="/inventory-list" className="close__link">
+                    <button className="close__button" onClick={onClose}>
                         <img className="close__img" src={close} alt="Close" />
                     </button>
-                </Link>
+                </div>
             </div>
             <div className="main">
                 <div className="delete-item">
-                    <h1 className="delete-item__title">Delete **Television** inventory item?</h1>
+                    <h1 className="delete-item__title">Delete {inventoryData.item_name} item?</h1>
                     <p className="delete-item__body">
-                        Please confirm that you’d like to delete **Television** from the inventory
-                        list. You won’t be able to undo this action.
+                        Please confirm that you’d like to delete {inventoryData.item_name} from the
+                        inventory list. You won’t be able to undo this action.
                     </p>
                 </div>
                 <div className="delete-button">
-                    <Link to="/inventory-list" className="delete-button__link">
-                        <button className="delete-button__cancel">Cancel</button>
-                    </Link>
-                    <Link to="/inventory-list" className="delete-button__link">
-                        {/*  SEND DELETE REQUEST */}
-                        <button className="delete-button__delete">Delete</button>
-                    </Link>
+                    <div to="/inventory-list" className="delete-button__link">
+                        <button className="delete-button__cancel" onClick={onClose}>
+                            Cancel
+                        </button>
+                    </div>
+                    <div to="/inventory-list" className="delete-button__link">
+                        <button className="delete-button__delete" onClick={handleDelete}>
+                            Delete
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
