c8a7c88c836347acd7c9f63824ea36aa1e0d8d35
Alex Howley <howley.alex@gmail.com>
2023-11-09 18:41:16 -0600

    Clean up styles and create main skeleton styles

--- src/Components/InventoryItemDetails/InventoryItemDetails.js
+++ src/Components/InventoryItemDetails/InventoryItemDetails.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Title from "../Title/Title";
 import "./InventoryItemDetails.scss";
 import axios from "axios";
 
@@ -16,39 +17,44 @@ function InventoryItemDetails({ id }) {
         getItem();
     }, [id]);
 
-    return (
+    return item.description ? (
         <>
-            {item.description ? (
-                <div className="item-details-container">
-                    <div className="leftside">
-                        <h4 className="item-details-container__title">ITEM DESCRIPTION:</h4>
-                        <p className="item-details-container__body">{item.description}</p>
-                        <h4 className="item-details-container__title">CATEGORY:</h4>
-                        <p className="item-details-container__body">{item.category}</p>
+            {" "}
+            <Title
+                pageTitle={item.item_name}
+                edit="Edit"
+                backUrl="/inventory"
+                handleEdit={() => {}}
+            />
+            <div className="item-details-container">
+                <div className="leftside">
+                    <h4 className="item-details-container__title">ITEM DESCRIPTION:</h4>
+                    <p className="item-details-container__body">{item.description}</p>
+                    <h4 className="item-details-container__title">CATEGORY:</h4>
+                    <p className="item-details-container__body">{item.category}</p>
+                </div>
+
+                <div className="test">
+                    <div className="rightside">
+                        <div className="rightside__status">
+                            <h4 className="item-details-container__title">STATUS:</h4>
+                            <p className="item-details-container__body">{item.status}</p>
+                        </div>
+
+                        <div className="rightside__quantity">
+                            <h4 className="item-details-container__title">QUANTITY:</h4>
+                            <p className="item-details-container__body"> {item.quantity} </p>
+                        </div>
                     </div>
-
-                    <div className="test">
-                        <div className="rightside">
-                            <div className="rightside__status">
-                                <h4 className="item-details-container__title">STATUS:</h4>
-                                <p className="item-details-container__body">{item.status}</p>
-                            </div>
-
-                            <div className="rightside__quantity">
-                                <h4 className="item-details-container__title">QUANTITY:</h4>
-                                <p className="item-details-container__body"> {item.quantity} </p>
-                            </div>
-                        </div>
-                        <div className="rightside__warehouse">
-                            <h4 className="item-details-container__title">WAREHOUSE:</h4>
-                            <p className="item-details-container__body">** Manhattan **</p>
-                        </div>
+                    <div className="rightside__warehouse">
+                        <h4 className="item-details-container__title">WAREHOUSE:</h4>
+                        <p className="item-details-container__body">** Manhattan **</p>
                     </div>
                 </div>
-            ) : (
-                <div>Item not found</div>
-            )}
+            </div>
         </>
+    ) : (
+        <div>Item not found</div>
     );
 }
 
--- src/Pages/Warehouses/Warehouses.js
+++ src/Pages/Warehouses/Warehouses.js
@@ -1,27 +1,12 @@
 import { useParams } from "react-router-dom";
 import "./Warehouses.scss";
-import InventoryItemDetails from "../../Components/InventoryItemDetails/InventoryItemDetails";
-import Title from "../../Components/Title/Title";
 import WarehouseList from "../../Components/WarehouseList/WarehouseList";
+import WarehouseDetails from "../../Components/WarehouseDetails/WarehouseDetails";
 
 function Warehouses() {
     const { id } = useParams();
-    return (
-        <div>
-            <Title
-                pageTitle="Invetory Details"
-                edit="Edit"
-                backUrl="/inventory"
-                handleEdit={() => {}}
-            />
-            {id !== undefined && <InventoryItemDetails id={id} />}
-        </div>
-    );
-    return (
-        <main class="warehouses">
-            <WarehouseList />
-        </main>
-    );
+    return <main className="warehouses">{!id ? <WarehouseList /> : <WarehouseDetails />}</main>;
+    return;
 }
 
 export default Warehouses;
