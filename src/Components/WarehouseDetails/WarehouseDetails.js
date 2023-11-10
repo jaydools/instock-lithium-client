4bd777b46fc58bd714d8bb45919c187f7f70da2e
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-10 11:26:37 -0800

    fully functional iventory list for each warehouse

--- src/Components/WarehouseDetails/WarehouseDetails.js
+++ src/Components/WarehouseDetails/WarehouseDetails.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Grid from "../Grid/Grid";
+import Title from "../Title/Title";
+import "./WarehouseDetails.scss";
 
 function WarehouseDetails({ warehouseId }) {
     const [warehouseFromId, setWarehouseFromId] = useState({});
     const [warehouseIdInventory, setWarehouseIdInventory] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchWarehouseId = async warehouseId => {
+        const fetchWarehouseFromId = async warehouseId => {
             try {
                 const response = await axios.get(
                     `${process.env.REACT_APP_BACKEND_URL}/api/warehouses/${warehouseId}`,
@@ -29,15 +34,66 @@ function WarehouseDetails({ warehouseId }) {
                 console.log(error);
             }
         };
-        fetchWarehouseId(warehouseId || 1);
+        fetchWarehouseFromId(warehouseId || 1);
         fetchWarehouseIdInventories(warehouseId);
     }, [warehouseId]);
+
     return (
         <>
             {warehouseFromId && warehouseIdInventory ? (
                 <>
-                    <div>title</div>
-                    <h3>{warehouseFromId.warehouse_name}</h3>
+                    <Title
+                        pageTitle={warehouseFromId.warehouse_name}
+                        edit="Edit"
+                        handleBack={() => navigate("/")}
+                        handleEdit={() => {}}
+                    />
+                    <div className="warehouse-details-container">
+                        <div>
+                            <h4 className="warehouse-details-container__title">
+                                WAREHOUSE ADDRESS:
+                            </h4>
+                            <div className="warehouse-details-container__address-container">
+                                <p className="warehouse-details-container__body">
+                                    {warehouseFromId.address},
+                                </p>
+                                <p className="warehouse-details-container__body">
+                                    {warehouseFromId.city}, {warehouseFromId.country}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="contact-container">
+                            <div className="contact-container__contact-name">
+                                <h4 className="item-details-container__title">CONTACT NAME:</h4>
+                                <p className="contact-container__body--padding">
+                                    {warehouseFromId.contact_name}
+                                </p>
+                                <p className="item-details-container__body">
+                                    {warehouseFromId.contact_position}
+                                </p>
+                            </div>
+                            <div>
+                                <h4 className="item-details-container__title">
+                                    CONTACT INFORMATION:
+                                </h4>
+                                <p className="contact-container__body--padding">
+                                    {warehouseFromId.contact_phone}
+                                </p>
+                                <p className="item-details-container__body">
+                                    {warehouseFromId.contact_email}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <Grid
+                        fieldNames={["item_name", "category", "status", "quantity"]}
+                        displayNamesMobile={["inventory item", "category", "Status", "Qty"]}
+                        displayNamesDesktop={["inventory item", "category", "Status", "quantity"]}
+                        records={warehouseIdInventory}
+                        linkToDetailsPage={"/inventory"}
+                        onEdit={id => console.log(`edited ${id}`)}
+                        onDelete={id => console.log(`deleted ${id}`)}
+                    />
                 </>
             ) : (
                 <p>"loading details..."</p>
