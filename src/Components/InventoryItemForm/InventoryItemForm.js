b389706c30db8bb0200ae547181080cdf01c85cb
Alex Howley <howley.alex@gmail.com>
2023-11-13 01:08:45 -0600

    Style inventory item form

--- src/Components/InventoryItemForm/InventoryItemForm.js
+++ src/Components/InventoryItemForm/InventoryItemForm.js
@@ -1,7 +1,18 @@
 import "./InventoryItemForm.scss";
 import Title from "../../Components/Title/Title";
 import { useEffect, useState } from "react";
+import failIcon from "../../Assets/Images/error-24px.svg";
 import axios from "axios";
+
+function FailureMessage({ hasFailedValidation }) {
+    return !hasFailedValidation ? (
+        <></>
+    ) : (
+        <p className="item-form__invalid">
+            <img src={failIcon} alt="" className="item-form__icon" /> This field is required
+        </p>
+    );
+}
 
 function InventoryItemForm({ handleBack, selectedInventoryID }) {
     const [fields, setFields] = useState({
@@ -18,7 +29,7 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
 
     const validateField = (field, value, updateState = true) => {
         const fieldWasValid = !invalidFields.includes(field);
-        let fieldIsValid = fields[field] !== "";
+        let fieldIsValid = value !== "";
 
         if (field === "quantity") {
             fieldIsValid = /^\d+$/.test(value);
@@ -87,17 +98,6 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
             );
             setCategories(categoryResponse.data);
             setWarehouses(warehouseResponse.data);
-
-            if (!selectedInventoryID) {
-                setFields({
-                    item_name: "",
-                    description: "",
-                    category: "",
-                    status: "In Stock",
-                    quantity: "",
-                    warehouse_id: `${warehouseResponse.data[0].id}`,
-                });
-            }
         };
         const getItemData = async () => {
             const { data } = await axios.get(
@@ -143,6 +143,7 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
                             value={fields.item_name}
                             onBlur={event => validateField("item_name", fields.item_name)}
                         />
+                        <FailureMessage hasFailedValidation={invalidFields.includes("item_name")} />
                     </label>
                     <label className="item-form__label">
                         Description
@@ -158,27 +159,41 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
                             value={fields.description}
                             onBlur={event => validateField("description", fields.description)}
                         ></textarea>
+                        <FailureMessage
+                            hasFailedValidation={invalidFields.includes("description")}
+                        />
                     </label>
                     <label className="item-form__label">
                         Category
                         <select
-                            className="item-form__select"
+                            className={`item-form__select ${
+                                invalidFields.includes("category")
+                                    ? "item-form__select--invalid"
+                                    : ""
+                            } ${!fields.category ? "item-form__select--placeholder" : ""}`}
                             name="category"
-                            onChange={event => handleFieldChange("category", event.target.value)}
+                            onChange={event => (
+                                handleFieldChange("category", event.target.value),
+                                validateField("category", event.target.value)
+                            )}
                             value={fields.category}
                         >
+                            <option value="" disabled>
+                                Please select
+                            </option>
                             {categories.map(category => (
                                 <option key={category} value={category}>
                                     {category}
                                 </option>
                             ))}
                         </select>
+                        <FailureMessage hasFailedValidation={invalidFields.includes("category")} />
                     </label>
                 </fieldset>
                 <fieldset className="item-form__fieldset">
                     <h2 className="item-form__subheading">Item Availability</h2>
                     <div className="item-form__radio-buttons">
-                        <p className="item-form__subheading">Status</p>
+                        <p className="item-form__label">Status</p>
                         <label className="item-form__radio-text">
                             <input
                                 type="radio"
@@ -213,19 +228,30 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
                     <label htmlFor="" className="item-form__label">
                         Warehouse
                         <select
-                            name="warehouse"
-                            className="item-form__select"
-                            onChange={event =>
-                                handleFieldChange("warehouse_id", event.target.value)
-                            }
+                            name="warehouse_id"
+                            className={`item-form__select ${
+                                invalidFields.includes("warehouse_id")
+                                    ? "item-form__select--invalid"
+                                    : ""
+                            } ${!fields.warehouse_id ? "item-form__select--placeholder" : ""}`}
+                            onChange={event => (
+                                handleFieldChange("warehouse_id", event.target.value),
+                                validateField("warehouse_id", event.target.value)
+                            )}
                             value={fields.warehouse_id}
                         >
+                            <option value="" disabled>
+                                Please select
+                            </option>
                             {warehouses.map(warehouse => (
                                 <option key={warehouse.id} value={warehouse.id}>
                                     {warehouse.warehouse_name}
                                 </option>
                             ))}
                         </select>
+                        <FailureMessage
+                            hasFailedValidation={invalidFields.includes("warehouse_id")}
+                        />
                     </label>
                     {fields.status === "In Stock" && (
                         <label className="item-form__label">
@@ -244,19 +270,24 @@ function InventoryItemForm({ handleBack, selectedInventoryID }) {
                                 onBlur={event => validateField("quantity", fields.quantity)}
                                 value={fields.quantity}
                             />
+                            <FailureMessage
+                                hasFailedValidation={invalidFields.includes("quantity")}
+                            />
                         </label>
                     )}
                 </fieldset>
-                <button
-                    className="item-form__button item-form__button--cancel"
-                    type="button"
-                    onClick={handleBack}
-                >
-                    Cancel
-                </button>
-                <button className="item-form__button item-form__button--update">
-                    {selectedInventoryID ? "Save" : "+ Add Item"}
-                </button>
+                <div className="item-form__buttons">
+                    <button
+                        className="item-form__button item-form__button--cancel"
+                        type="button"
+                        onClick={handleBack}
+                    >
+                        Cancel
+                    </button>
+                    <button className="item-form__button item-form__button--update">
+                        {selectedInventoryID ? "Save" : "+ Add Item"}
+                    </button>
+                </div>
             </form>
         </section>
     );
